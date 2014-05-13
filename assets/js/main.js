07083f5ef8be0277848f0cc03aa0f4da4ebbd9f0
Smbat Yeranyan <smbat.yeranyan@gmail.com>
2014-05-13 15:58:45 +0200

    Optimization

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,76 +1,5 @@
 var socket = io.connect('http://localhost:3456');
 
-$(document).ready(function(){
-	var Max =0;
-	$.get("http://api.flickr.com/services/rest/", {
-    method: "flickr.photos.search",
-    api_key: "90485e931f687a9b9c2a66bf58a3861a",
-    text: "car",
-    safe_search: 1,
-    content_type: 1,
-    sort: "relevance",
-    per_page: 32
-  }, function(data, status, request) {
-    var photos = request.responseXML.getElementsByTagName("photo");
- 
-	for (var i = 0, photo; photo = photos[i]; i++){
-		Max++;
-	/*$(".history").append("<figure style='background-image: url("+construcURL(photo)+");'></figure>");*/
-	}
-  }
-);
-
-	function construcURL(photo){
-    return "http://farm" + photo.getAttribute("farm") +
-        ".static.flickr.com/" + photo.getAttribute("server") +
-        "/" + photo.getAttribute("id") +
-        "_" + photo.getAttribute("secret") +
-        "_m.jpg";
-   }
-
-//$("article.history").addClass("sway");
-/*   var mainTimer= setInterval(function(){
-
-	if ($("article.history").hasClass("sway")){
-
-		console.log("hasClass");
-		$("article.history").removeClass("sway");
-	}else{
-		console.log("!hasClass");
-		$("article.history").addClass("sway");
-
-
-	}
-},30000);   
-*/
-   var cycle =0;
-   var select= setInterval(function(){
-
-
-	$("article.history figure").removeClass("selected");
-	for (var i = cycle; i < cycle + 20; i++) {
-		$("article.history").find("figure").eq(i).addClass("selected");
-	}
-
-	if ($("article.history").hasClass("sway")){
-		if (cycle >= Max){
-			return;
-		}
-			cycle +=20;
-	}else{
-		if (cycle <= 0){
-			return;
-		}
-		cycle -=20;
-
-
-	}
-
-		
-		
-
-   },3000);
-});
 
 var sout;
 var theFinalCountdown = 10;
@@ -120,7 +49,7 @@ socket.on("photos", function(data){
 	console.log(data);
 	for (var i = 0, photo; photo = data[i]; i++){
 		currentPhotos.push(photo);
-		$(".history").append("<figure file='"+data[i]+"'><img src='/images/"+photo+"'/></figure>");
+		addImg(photo);
 	}
 });
 
@@ -132,7 +61,7 @@ socket.on("added", function(photo){
 		$(".modal .content img").attr("src","/images/"+photo);
 		currentPhotos.push(photo);
 		hand();
-		$(".history").append("<figure file='"+photo+"'><img src='/images/"+photo+"'/></figure>");
+		addImg(photo);
 	}
 
 });
@@ -147,4 +76,7 @@ socket.on("removed", function(photo){
 
 });
 
+function addImg(img){
+	$(".history").append("<figure file='"+img+"'><img width='480' height='270' src='/images/"+img+"'/></figure>");
 
+}
